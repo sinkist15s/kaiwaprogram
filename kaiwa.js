894d29800b934e75b739dbbefb64d989b833f9f8
1503008 <1503008>
2016-07-22 09:21:43 +0900

    上の選択肢消しました。 画像追加

--- kaiwa.js
+++ kaiwa.js
@@ -2,22 +2,8 @@
 	HTMLでフォームに入力された言葉をどうやって返すか？*/
 //↓プログラム書き場
 
-//選択肢返答サンプル
-function goScript(num) {
-	num++;
-	switch(num){
-		case 1:
-			alert("こんにちは!");
-			break;
-		case 2:
-			alert("こんばんは!");
-			break;
-		
-	}
-	document.write("更新してください");
 
-}
-/*「こんにちは」か「こんばんは」に"Hello"と返す処理*/
+
 function onButtonClick() {
           target = document.getElementById("output");
           
