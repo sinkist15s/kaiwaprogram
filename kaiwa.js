88fa6eece41d27c15592f489229b8b9ff9f66951
1503008 <1503008>
2016-07-25 09:52:12 +0900

    完成（仮）

--- kaiwa.js
+++ kaiwa.js
@@ -18,7 +18,7 @@ function onButtonClick() {
 		 */
 			if(str.indexOf("こんにちは")!=-1 || str.indexOf("こんばんは")!=-1){
 				document.write("Hello");
-		}else if(str.indexOf("おはよ")!=-1){
+		}else if(str.indexOf("おはよ")!=-1 || str.indexOf("おはようございます")!=-1)){
 			document.write("おはようなぎ");
 		}else if(str.indexOf("おまえはもう死んでいる")!=-1){
 			document.write("なにぃ！！　い！？　いが、いぎ、いぐ　いけごあぁ！！");
@@ -37,8 +37,153 @@ function onButtonClick() {
 							+hour.getMinutes()+"分"+
 								hour.getSeconds()+"秒です。");
 			
-			}else if(str.indexOf("おはようございます")!=-1){
-			document.write("おはよう");
+			}else if(str.indexOf("ありがとう")!=-1){
+			document.write("どういたしまして");
+		}else if(str.indexOf("さようなら")!=-1 || str.indexOf("さよなら")!=-1){
+			document.write("さようなら");
+		}else if(str.indexOf("ごめん")!=-1){
+			document.write("こちらこそごめんなさい");
+		}else if(str.indexOf("おめでとう")!=-1){
+document.write("どうもありがとう");
+}else if(str.indexOf("元日まであと何日？")!=-1){
+				count = Math.ceil( (ganjitu.getTime() - now.getTime()) / (24*60*60*1000) );
+				if(count>0){
+					document.write("元日まであと"+count+"日です");
+				}else if(count==0){
+					document.write("今日です！");
+				}else if(count<0){
+					document.write("もう過ぎました");
+				}
+				
+			}else if(str.indexOf("成人の日まであと何日？")!=-1){
+				count = Math.ceil( (seijin.getTime() - now.getTime()) / (24*60*60*1000) );
+				if(count>0){
+					document.write("成人の日まであと"+count+"日です");
+				}else if(count==0){
+					document.write("今日です！");
+				}else if(count<0){
+					document.write("もう過ぎました");
+				}
+				
+			}else if(str.indexOf("春分の日まであと何日？")!=-1){
+				count = Math.ceil( (shunbun.getTime() - now.getTime()) / (24*60*60*1000) );
+				if(count>0){
+					document.write("春分の日まであと"+count+"日です");
+				}else if(count==0){
+					document.write("今日です！");
+				}else if(count<0){
+					document.write("もう過ぎました");
+				}
+				
+			}else if(str.indexOf("昭和の日まであと何日？")!=-1){
+				count = Math.ceil( (shouwa.getTime() - now.getTime()) / (24*60*60*1000) );
+				if(count>0){
+					document.write("昭和の日まであと"+count+"日です");
+				}else if(count==0){
+					document.write("今日です！");
+				}else if(count<0){
+					document.write("もう過ぎました");
+				}
+				
+			}else if(str.indexOf("憲法記念日まであと何日？")!=-1){
+				count = Math.ceil( (kenpou.getTime() - now.getTime()) / (24*60*60*1000) );
+				if(count>0){
+					document.write("憲法記念日まであと"+count+"日です");
+				}else if(count==0){
+					document.write("今日です！");
+				}else if(count<0){
+					document.write("もう過ぎました");
+				}
+				
+			}else if(str.indexOf("建国記念日まであと何日？")!=-1){
+				count = Math.ceil( (midori.getTime() - now.getTime()) / (24*60*60*1000) );
+				if(count>0){
+					document.write("建国記念日まであと"+count+"日です");
+				}else if(count==0){
+					document.write("今日です！");
+				}else if(count<0){
+					document.write("もう過ぎました");
+				}
+				
+			}else if(str.indexOf("子供の日まであと何日？")!=-1){
+				count = Math.ceil( (kodomo.getTime() - now.getTime()) / (24*60*60*1000) );
+				if(count>0){
+					document.write("子供の日まであと"+count+"日です");
+				}else if(count==0){
+					document.write("今日です！");
+				}else if(count<0){
+					document.write("もう過ぎました");
+				}
+				
+			}else if(str.indexOf("海の日まであと何日？")!=-1){
+				count = Math.ceil( (umi.getTime() - now.getTime()) / (24*60*60*1000) );
+				if(count>0){
+					document.write("海の日まであと"+count+"日です");
+				}else if(count==0){
+					document.write("今日です！");
+				}else if(count<0){
+					document.write("もう過ぎました");
+				}
+				
+			}else if(str.indexOf("敬老の日まであと何日？")!=-1){
+				count = Math.ceil( (keirou.getTime() - now.getTime()) / (24*60*60*1000) );
+				if(count>0){
+					document.write("敬老の日まであと"+count+"日です");
+				}else if(count==0){
+					document.write("今日です！");
+				}else if(count<0){
+					document.write("もう過ぎました");
+				}
+				
+			}else if(str.indexOf("秋分の日まであと何日？")!=-1){
+				count = Math.ceil( (shubun.getTime() - now.getTime()) / (24*60*60*1000) );
+				if(count>0){
+					document.write("秋分の日まであと"+count+"日です");
+				}else if(count==0){
+					document.write("今日です！");
+				}else if(count<0){
+					document.write("もう過ぎました");
+				}
+				
+			}else if(str.indexOf("体育の日まであと何日？")!=-1){
+				count = Math.ceil( (taiiku.getTime() - now.getTime()) / (24*60*60*1000) );
+				if(count>0){
+					document.write("体育の日まであと"+count+"日です");
+				}else if(count==0){
+					document.write("今日です！");
+				}else if(count<0){
+					document.write("もう過ぎました");
+				}
+				
+			}else if(str.indexOf("文化の日まであと何日？")!=-1){
+				count = Math.ceil( (bunka.getTime() - now.getTime()) / (24*60*60*1000) );
+				if(count>0){
+					document.write("文化の日まであと"+count+"日です");
+				}else if(count==0){
+					document.write("今日です！");
+				}else if(count<0){
+					document.write("もう過ぎました");
+				}
+				
+			}else if(str.indexOf("勤労感謝の日まであと何日？")!=-1){
+				count = Math.ceil( (kinrou.getTime() - now.getTime()) / (24*60*60*1000) );
+				if(count>0){
+					document.write("勤労感謝の日まであと"+count+"日です");
+				}else if(count==0){
+					document.write("今日です！");
+				}else if(count<0){
+					document.write("もう過ぎました");
+				}
+				
+			}else if(str.indexOf("天皇誕生日まであと何日？")!=-1){
+				count = Math.ceil( (tennou.getTime() - now.getTime()) / (24*60*60*1000) );
+				if(count>0){
+					document.write("天皇誕生日まであと"+count+"日です");
+				}else if(count==0){
+					document.write("今日です！");
+				}else if(count<0){
+					document.write("もう過ぎました");
+				}
 			}else{
 			document.write("すみません何言っているのかわかりません");
       }
